5f3a26fbfd66e46221638935f7b6f41442a0a862
matarof <sintarof@terra.dti.ne.jp>
2015-07-28 18:46:45 +0900

    waveform draw implemented

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxVoicefile = new System.Windows.Forms.TextBox();
+            this.comboBoxAudioIF = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 291);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(801, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxVoicefile
+            // 
+            this.textBoxVoicefile.Location = new System.Drawing.Point(801, 37);
+            this.textBoxVoicefile.Name = "textBoxVoicefile";
+            this.textBoxVoicefile.Size = new System.Drawing.Size(226, 19);
+            this.textBoxVoicefile.TabIndex = 2;
+            // 
+            // comboBoxAudioIF
+            // 
+            this.comboBoxAudioIF.FormattingEnabled = true;
+            this.comboBoxAudioIF.Location = new System.Drawing.Point(801, 136);
+            this.comboBoxAudioIF.Name = "comboBoxAudioIF";
+            this.comboBoxAudioIF.Size = new System.Drawing.Size(197, 20);
+            this.comboBoxAudioIF.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(32, 379);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(712, 256);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(801, 379);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(425, 256);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 705);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBoxAudioIF);
+            this.Controls.Add(this.textBoxVoicefile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxVoicefile;
+        private System.Windows.Forms.ComboBox comboBoxAudioIF;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using MathNet.Numerics.IntegralTransforms;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using NAudio.CoreAudioApi;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private string voice;
+
+        int df;  //周波数応答関数計算ステップ(Hz)
+        int audioLatency = 50;
+        private int streamLength = 30000;   //初期値29650
+        private IWavePlayer WaveOutDevice = null;
+        private WaveStream wavStream = null;
+        private FilteredWaveStream outputStream;
+        public double[] inputSampleStream;
+        bool isPlay;
+        int melFiltNum = 20;
+        int[][] mfbIndices;
+        double[][] tBank;
+        public double[] melSpectrum;
+        public double[] MFCC;
+
+        double log2 = Math.Log(2);
+
+
+
+        Graphics g1;
+        Graphics g2;
+        Graphics g3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            voice = "C:\\Workspace\\GlottalSource2.wav";
+
+            inputSampleStream = new double[streamLength];
+            //filteredSampleStream = new double[streamLength];
+            //postprocessStream = new double[streamLength - faderWidth];
+
+
+
+            Bitmap bmp1 = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = bmp1;
+            g1 = Graphics.FromImage(pictureBox1.Image);
+
+            Bitmap bmp2 = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            pictureBox2.Image = bmp2;
+            g2 = Graphics.FromImage(pictureBox2.Image);
+
+            Bitmap bmp3 = new Bitmap(pictureBox3.Size.Width, pictureBox3.Size.Height);
+            pictureBox3.Image = bmp3;
+            g3 = Graphics.FromImage(pictureBox3.Image);
+
+            setWaveFile1();
+            SystemUpdate();
+
+            melFiltNum = 20;
+            isPlay = false;
+        }
+
+        private void InitializeWasapiControls()
+        {
+            var enumerator = new MMDeviceEnumerator();
+            var endPoints = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            var comboItems = new List<WasapiDeviceComboItem>();
+            foreach (var endPoint in endPoints)
+            {
+                var comboItem = new WasapiDeviceComboItem();
+                comboItem.Description = string.Format("{0} ({1})", endPoint.FriendlyName, endPoint.DeviceFriendlyName);
+                comboItem.Device = endPoint;
+                comboItems.Add(comboItem);
+            }
+            comboBoxAudioIF.DisplayMember = "Description";
+            comboBoxAudioIF.ValueMember = "Device";
+            comboBoxAudioIF.DataSource = comboItems;
+        }
+
+        private IWavePlayer CreateDevice(int id)
+        {
+                    WasapiOut outputDevice = new WasapiOut(
+                (MMDevice)comboBoxAudioIF.SelectedValue, AudioClientShareMode.Shared, false, audioLatency);
+            return outputDevice as IWavePlayer;
+
+        }
+
+        private void ClearAudioDevice()
+        {
+            if (WaveOutDevice != null)
+            {
+                WaveOutDevice.Stop();
+                WaveOutDevice.Dispose();
+            }
+            if (wavStream != null)
+            {
+                wavStream.Dispose();
+            }
+            WaveOutDevice = null;
+        }
+
+        class WasapiDeviceComboItem
+        {
+            public string Description { get; set; }
+            public MMDevice Device { get; set; }
+        }
+
+        class FilteredWaveStream : ISampleProvider
+        {
+
+            private readonly WaveFormat waveFormat;
+
+            private int readIndex;
+            private int readOffset;
+            private int readStart;
+            private int readEnd;
+            private double[] out_Array;
+
+
+            public FilteredWaveStream(int sampleRate, double[] data)
+            {
+                waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, 1);
+                Gain = 1;
+                out_Array = data;
+                readOffset = 3160; //////////読み出し開始位置 3100
+
+
+                for (int i = 0; i < out_Array.Length; i++)　///////////////////////////////////// ゼロクロス位置強制
+                {
+                    if ((out_Array[i + readOffset] < 0) && (out_Array[i + readOffset + 1] > 0))
+                    {
+                        readStart = i + readOffset + 1;
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                for (int i = 0; i < out_Array.Length; i++)
+                {
+                    if ((out_Array[out_Array.Length - 1 - i] > 0) && (out_Array[out_Array.Length - 2 - i] < 0))
+                    {
+                        readEnd = out_Array.Length - 2 - i;
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                readIndex = readStart;
+            }
+
+            public WaveFormat WaveFormat
+            {
+                get { return waveFormat; }
+            }
+
+            public double Gain { get; set; }
+
+
+            public int Read(float[] buffer, int offset, int count)
+            {
+                int outIndex = offset;
+
+                for (int sampleCount = 0; sampleCount < count; sampleCount++)
+                {
+
+                    buffer[outIndex++] = (float)((float)(out_Array[readIndex]) / 32767 / 1000 * Gain);
+
+                    readIndex++;
+                    if (readIndex >= readEnd)
+                    {
+                        readIndex = readStart;
+                    }
+                }
+                return count;
+            }
+
+
+        }
+
+
+        private void setWaveFile1()
+        {
+
+            FileInfo fi = new FileInfo(this.voice);
+            if (!fi.Exists)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "wavファイル|*.wav";
+                openFileDialog1.Title = "Select a glottal source File";
+
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.voice = openFileDialog1.FileName;
+                }
+                this.textBoxVoicefile.Text = this.voice;
+                openFileDialog1.Dispose();
+            }
+
+            string fileExt = fi.Extension;
+
+            if (fileExt.ToLower() == ".wav") wavStream = new WaveFileReader(this.voice);
+            else return;
+
+            byte[] byteArray = new byte[streamLength * 2];
+            wavStream.Read(byteArray, 0, streamLength * 2);
+
+            for (int i = 0; i < byteArray.Length / 2; i++)
+            {
+                inputSampleStream[i] = BitConverter.ToInt16(byteArray, i * 2);
+                
+            }
+            int j;
+        }
+
+        private int iMel(double m)
+        {
+            int f = (int)(1000 * (Math.Exp(m * log2 / 1000) - 1));
+            return f;
+        }
+
+        private double fMel(int f)
+        {
+            double m = (1000 / log2) * Math.Log(f / 1000 + 1);
+            return m;
+        }
+
+        private void initTBank(int mn, int maxf)  //mnフィルタバンク数　maxf フィルタバンク最大周波数
+        {
+
+            double maxm = fMel(maxf);
+            double melFiltWidth = maxm / (melFiltNum / 2 + 0.5);
+
+            mfbIndices = new int[mn][];
+            tBank = new double[mn][];
+
+            for (int i = 0; i < melFiltNum; i++)
+            {
+                double ma = melFiltWidth * i * 0.5;
+                double mb = melFiltWidth * (i * 0.5 + 1);
+
+                int fai = (int)(iMel(ma) / df);
+                int fbi = (int)(iMel(mb) / df);
+
+                mfbIndices[i] = new int[] { fbi - fai + 1, fai, fbi };
+
+
+                tBank[i] = new double[mfbIndices[i][0]];
+                for (int j = 0; j < tBank[i].Length; j++)
+                {
+                    tBank[i][j] = bartlett(j, mfbIndices[i][0]);
+
+                }
+
+                //tBank[i] = new double[(int)sampleRate / 2 / df];
+                //for (int j = 0; j < tBank[i].Length; j++)
+                //{
+                //    if (j >= fai && j < fbi)
+                //    {
+                //        tBank[i][j] = bartlett(j - fai, fbi - fai);
+                //    }
+                //    else
+                //    {
+                //        tBank[i][j] = 0;
+                //    }
+                //}
+
+            }
+
+        }
+
+        private double bartlett(int n, int l)
+        {
+            double i;
+
+            if (n <= l / 2)
+            {
+                i = 2.0 * n / l;
+            }
+            else
+            {
+                i = 2.0 - 2.0 * n / l;
+            }
+            return i;
+
+        }
+
+        public void melFilter(double[] data)
+        {
+
+            Parallel.For(0, melFiltNum, i =>
+            {
+                melSpectrum[i] = 0;
+
+                for (int j = 0; j < mfbIndices[i][0]; j++)
+                {
+                    melSpectrum[i] += data[mfbIndices[i][1] + j] * tBank[i][j];
+
+                }
+
+            });
+        }
+
+
+        public double[] dct_ii(double[] data)  //////////////////////////////////////////DCT-II bruteforce
+        {
+            double[] c = new double[data.Length];
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                double a = Math.PI * i / data.Length;
+
+                Parallel.For(1, data.Length, j =>
+                {
+                    c[i] += data[j] * Math.Cos(a * (j + 0.5));
+                });
+            }
+
+            return c;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "wavファイル|*.wav";
+            openFileDialog1.Title = "Select a glottal source File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.voice = openFileDialog1.FileName;
+            }
+            this.textBoxVoicefile.Text = this.voice;
+            setWaveFile1();
+
+            openFileDialog1.Dispose();
+        }
+            private   void waveformDraw(Graphics g, double[] data)
+    {
+
+     Pen pen1 = new Pen(Color.Black);
+            for (int i = 2; i < 800; i++)
+            {
+                g.DrawLine(
+                    pen1,
+                    (int)(i - 1),
+                    (int)(200 - data[i * 30 + 1000] / 100),
+                    (int)(i),
+                    (int)(200 - data[(i + 1) * 30 + 1000] / 100)
+                    );
+            }
+    }
+            private void spectrumDraw(Graphics g, double[] data)
+            {
+                Pen pen1 = new Pen(Color.Crimson);
+
+                if (radioButton2.Checked)
+                {
+                    hsvColor.H = hue_ct;
+                    pen1.Color = hsvColor.ToRGB();
+                    hue_ct += 5;
+                    if (hue_ct == 360)
+                    {
+                        hue_ct = 0;
+                    }
+                }
+
+
+                for (int i = 2; i < data.GetLength(0); i++)
+                {
+                    g.DrawLine(
+                        pen1,
+                        (int)((i - 1) * 2),
+                        (int)(Math.Log10(data[i - 1]) * -100 + 200),
+                        (int)(i * 2),
+                        (int)(Math.Log10(data[i]) * -100 + 200)
+                        );
+                }
+
+                Pen pen2 = new Pen(Color.LightGray);
+                g.DrawLine(pen2, 0, 200, pictureBox1.Width, 200);
+                for (int i = 1; i < 100; i += 10)
+                {
+                    g.DrawLine(pen2, 0, (int)(Math.Log10(i) * -100 + 200), pictureBox1.Width, (int)(Math.Log10(i) * -100 + 200));
+                }
+
+                for (int i = 1; i < 14; i++)
+                {
+                    g.DrawLine(pen2, i * 100, 0, i * 100, pictureBox1.Height);
+                }
+
+                Font fnt = new Font("Arial", 10);
+                g.DrawString("1000", fnt, Brushes.Gray, 185, 210);
+                g.DrawString("2000", fnt, Brushes.Gray, 385, 210);
+                g.DrawString("3000", fnt, Brushes.Gray, 585, 210);
+                g.DrawString("4000", fnt, Brushes.Gray, 785, 210);
+                g.DrawString("5000", fnt, Brushes.Gray, 985, 210);
+                g.DrawString("6000", fnt, Brushes.Gray, 1185, 210);
+
+            }
+
+
+
+        private void SystemUpdate()
+        {
+            g1.Clear(pictureBox1.BackColor);
+            waveformDraw(g1, inputSampleStream);
+            pictureBox1.Refresh();
+
+
+
+
+        }
+
+    }
+
+
+}
